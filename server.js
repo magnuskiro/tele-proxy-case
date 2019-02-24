beeb86eed985181201605d2314123b4b5b78beeb
Magnus Kirø <magnuskiro@gmail.com>
2019-02-24 16:38:03 +0100

    working server

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const express = require('express');
 
 // Constants
@@ -8,8 +7,30 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world - virk!\n');
+});
+
+const request = require('request');
+
+const apiUrl = "http://api.nilu.no";
+
+app.get("/*", function(req, res){ 
+    console.log(req.originalUrl);
+    
+    request(apiUrl+req.originalUrl, function (error, response, body) {
+        console.log('error:', error); 
+        console.log('statusCode:', response && response.statusCode); 
+
+        if( response.statusCode === 200 ){
+          //console.log('body:', body); 
+          res.send(body);
+        }else {
+          res.send("Unsupported request. Look at https://api.nilu.no/ for api specification. "
+             + "Response from api: " + body);
+        }
+    });
 });
 
 app.listen(PORT, HOST);
