8fdf1f5bb64e3973d74f6d9708c90302c6264996
Magnus Kirø <magnuskiro@gmail.com>
2019-02-24 18:05:51 +0100

    finished proxy, super simple

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 'use strict';
+
 const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
+const fs = require('fs');
+const util = require('util');
 const app = express();
-
-app.get('/hello', (req, res) => {
-  res.send('Hello world - virk!\n');
-});
-
 const request = require('request');
 
+const PORT = 8080;
+const HOST = '0.0.0.0';
 const apiUrl = "http://api.nilu.no";
 
+// storage / persistence
+function storeRes(response){
+   fs.appendFile('storage/db', util.inspect(response), (err) => {  
+       if (err) throw err;
+   });
+};
+
+app.get("/test", function(req, res){
+    res.send("testpage");
+}); 
+
+// proxy
 app.get("/*", function(req, res){ 
     console.log(req.originalUrl);
     
@@ -25,6 +31,7 @@ app.get("/*", function(req, res){
 
         if( response.statusCode === 200 ){
           //console.log('body:', body); 
+		  storeRes(response);
           res.send(body);
         }else {
           res.send("Unsupported request. Look at https://api.nilu.no/ for api specification. "
